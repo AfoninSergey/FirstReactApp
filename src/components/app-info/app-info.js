4a7420595c2114727cd03a2a49a3810de2f026a1
Gray Spaer <afoninsergey@mail.ru>
2023-01-13 13:08:26 +0300

    First commit Empty template

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,16 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+  return (
+    <div className="app-ifo">
+      <h1>
+        Учет сотрудников компании <br />
+        <b>ООО "РОСТТЕХЗАПЧАСТЬ"</b>
+      </h1>
+      <h2>Общее число сотрудников:</h2>
+      <h2>Премию получат:</h2>
+    </div>
+  );
+};
+
+export default AppInfo;
