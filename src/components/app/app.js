0db9f0b8e6bf7481c013ccead67d8e48bb6a6809
Gray Spaer <afoninsergey@mail.ru>
2023-01-16 15:14:59 +0300

    Add emloyeesData and props

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,4 @@
+//*Application Start
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
@@ -5,7 +6,17 @@ import EmployeesList from "../employees-list/employees-list";
 import EmployeesAddForm from "../employees-add-form/employees-add-form";
 import "./app.css";
 
+
+
 const App = () => {
+  //Эмуляция данных с сервера
+const employeesData = [
+  {name: "Gray Spaer", salary: 1000000, increase: true, id: 1},
+  {name: "Vovka Govno", salary: 1001, increase: false, id: 2},
+  {name: "Natasha Telefon", salary: 10, increase: true, id: 3},
+  {name: "Tanya Bux", salary: 150, increase: false, id: 4},
+]
+
   return (
     <div className="app">
       <AppInfo />
@@ -13,10 +24,35 @@ const App = () => {
         <SearchPanel/>
         <AppFilter/>
       </div>
-      <EmployeesList/>
+      <EmployeesList empData = {employeesData}/>
       <EmployeesAddForm/>
     </div>
   );
 };
 
 export default App;
+//*/ //Application End
+
+/*Lessons Start
+import "./app.css";
+
+const WhoAmI = (props) => {
+  return (
+    <div>
+      <h1>My name is <b> {props.name}</b>, Surname - <b>{props.surname}</b></h1>
+      <a href="N"><b>{props.link}</b></a>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="app">
+      <WhoAmI name="Gray" surname="Spaer" link="grayspaer.com"/>
+      <WhoAmI name="ASPiRe" surname="Gray" link="aspiregray.com"/>
+    </div>
+  );
+};
+
+export default App;
+*///Lessons End
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,14 +1,12 @@
 import EmployeesListItem from "../employees-list-item/employees-list-item";
 import "./employees-list.css";
 
-const EmployeesList = () => {
-  return(
-    <ul className="app-list list-group">
-      <EmployeesListItem/>
-      <EmployeesListItem/>
-      <EmployeesListItem/>
-    </ul>
-  )
+const EmployeesList = ({ empData }) => {
+  const employeesComponents = empData.map(({ id, ...otherProps }) => {
+    return <EmployeesListItem key={id} {...otherProps} />;
+  });
+
+  return <ul className="app-list list-group">{employeesComponents}</ul>;
 };
 
 export default EmployeesList;
