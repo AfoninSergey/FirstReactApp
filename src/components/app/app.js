7ad7d352051a34427871514ddeb237e6f69d5b2b
Gray Spaer <afoninsergey@mail.ru>
2023-01-17 11:22:09 +0300

    Add states, change  increase btn, rise btn

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -11,10 +11,10 @@ import "./app.css";
 const App = () => {
   //Эмуляция данных с сервера
 const employeesData = [
-  {name: "Gray Spaer", salary: 1000000, increase: true, id: 1},
-  {name: "Vovka Govno", salary: 1001, increase: false, id: 2},
-  {name: "Natasha Telefon", salary: 10, increase: true, id: 3},
-  {name: "Tanya Bux", salary: 150, increase: false, id: 4},
+  {name: "Gray Spaer", salary: 1000000, increase: true, rise: true, id: 1},
+  {name: "Vovka Govno", salary: 1001, increase: false, rise: false, id: 2},
+  {name: "Natasha Telefon", salary: 10, increase: true, rise: false, id: 3},
+  {name: "Tanya Bux", salary: 150, increase: false, rise: false, id: 4},
 ]
 
   return (
@@ -31,28 +31,63 @@ const employeesData = [
 };
 
 export default App;
-//*/ //Application End
+/*/ //Application End
 
 /*Lessons Start
+import { Component } from "react";
 import "./app.css";
 
-const WhoAmI = (props) => {
-  return (
-    <div>
-      <h1>My name is <b> {props.name}</b>, Surname - <b>{props.surname}</b></h1>
-      <a href="N"><b>{props.link}</b></a>
-    </div>
-  );
-};
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: 39
+    }
+    this.showThis = this.showThis.bind(this)
+  }
+
+  minusAge = () => {
+    this.setState(({age}) => ({age: age - 1}))
+  }
+
+  showThis() {
+    console.log(this);
+  }
+
+  commitInputNumberValue = (e) => {
+    console.log(e.target.value);
+  }
+
+  render() {
+    const { name, surname, link } = this.props;
+    const {age} = this.state
+
+    return (
+      <div>
+        <button onClick={this.showThis}>This</button>
+        <button onClick={this.minusAge}>---</button>
+        <h1>
+          My name is <b> {name}</b>, Surname - <b>{surname}</b>, Age: <b>{age}</b>
+        </h1>
+        <a href="N">
+          <b>{link}</b>
+        </a>
+        <form>
+          <input type="number" onChange={this.commitInputNumberValue}/>
+        </form>
+      </div>
+    );
+  }
+}
 
 const App = () => {
   return (
     <div className="app">
-      <WhoAmI name="Gray" surname="Spaer" link="grayspaer.com"/>
-      <WhoAmI name="ASPiRe" surname="Gray" link="aspiregray.com"/>
+      <WhoAmI name="Gray" surname="Spaer" link="grayspaer.com" />
+      <WhoAmI name="ASPiRe" surname="Gray" link="aspiregray.com" />
     </div>
   );
 };
 
 export default App;
-*///Lessons End
+*/ //Lessons End
--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,26 +1,57 @@
+import { Component } from "react";
 import "./employees-list-item.css";
 
-const EmployeesListItem = ({name, salary, increase}) => {
-  return (
-    <li className={"list-group-item d-flex justify-content-between" + (increase ? " increase" : "")}>
-      <span className="list-group-item-label">{name}</span>
-      <input
-        type="text"
-        className="list-group-item-input"
-        defaultValue={`$${salary}`}
-      />
-      <div className="d-flex justify-content-center align-items-center">
-        <button type="button" className="btn-cookie btn-sm ">
-          <i className="fas fa-cookie"></i>
-        </button>
+class EmployeesListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      increase: this.props.increase,
+      rise: this.props.rise
+    };
+  }
 
-        <button type="button" className="btn-trash btn-sm ">
-          <i className="fas fa-trash"></i>
-        </button>
-        <i className="fas fa-star"></i>
-      </div>
-    </li>
-  );
-};
+  onToggleIncrease = () => {
+    this.setState(({ increase }) => ({ increase: !increase }));
+  };
+
+  onToggleRise = () => {
+    this.setState(({rise}) => ({rise: !rise}))
+  };
+
+  render() {
+    const { name, salary} = this.props;
+    const { increase, rise  } = this.state;
+    return (
+      <li
+        className={
+          "list-group-item d-flex justify-content-between" +
+          (increase ? " increase" : "") +
+          (rise ? " like" : "")
+        }
+      >
+        <span className="list-group-item-label" onClick={this.onToggleRise}>{name}</span>
+        <input
+          type="text"
+          className="list-group-item-input"
+          defaultValue={`$${salary}`}
+        />
+        <div className="d-flex justify-content-center align-items-center">
+          <button
+            type="button"
+            className="btn-cookie btn-sm"
+            onClick={this.onToggleIncrease}
+          >
+            <i className="fas fa-cookie"></i>
+          </button>
+
+          <button type="button" className="btn-trash btn-sm ">
+            <i className="fas fa-trash"></i>
+          </button>
+          <i className="fas fa-star"></i>
+        </div>
+      </li>
+    );
+  }
+}
 
 export default EmployeesListItem;
